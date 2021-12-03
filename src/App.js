5024c2d1e846d80858d9dd0b3647449e294b0404
uzair-suria <uzair.a.suria@outlook.com>
2021-12-04 00:09:51 +0500

    Added Header component to App Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
+import Header from "./components/Header/Header";
 import HomePage from "./pages/Homepage/Homepage";
 import Shop from "./pages/Shop/Shop";
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<Shop />} />
