7dd07dc53ce19d129e4fd6f8b4850d8adaa45991
uzair-suria <uzair.a.suria@outlook.com>
2021-12-13 16:21:57 +0500

    added Navigation for the hats page

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-
+import { useLocation, useNavigate } from "react-router-dom";
 import "./MenuItem.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  // console.log(linkUrl);
+  const location = useLocation();
+  const navigate = useNavigate();
+  console.log(location.pathname);
   return (
     <div
       style={{ backgroundImage: `url(${imageUrl})` }}
-      className={`menu-item ${size ? "large" : ""}`}
+      className={`menu-item ${size}`}
+      onClick={() => navigate(`${location.pathname}${linkUrl}`)}
     >
       <div className="content">
         <h1 className="title">{title}</h1>
