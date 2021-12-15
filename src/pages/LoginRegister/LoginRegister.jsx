e5cd27bc715b1892a7ffbc2efb3155b39fbed1ed
uzair-suria <uzair.a.suria@outlook.com>
2021-12-15 22:12:56 +0500

    New page for Login and Registration

---
+++ src/pages/LoginRegister/LoginRegister.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "../../components/Login/Login";
+
+const LoginRegister = () => {
+  return (
+    <>
+      <div className="login-register">
+        <Login />
+      </div>
+    </>
+  );
+};
+
+export default LoginRegister;
