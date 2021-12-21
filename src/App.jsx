072b9df71955ef05f6077224a59608d395c4c35d
uzair-suria <uzair.a.suria@outlook.com>
2021-12-21 16:54:01 +0500

    updated styling and added redirection to home when accessing login page if the user is logged in

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { onSnapshot } from "firebase/firestore";
 import React from "react";
 import { connect } from "react-redux";
 import { Route, Routes } from "react-router";
+import { Navigate } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/Homepage/Homepage";
@@ -50,15 +51,28 @@ class App extends React.Component {
           <Route path="/" element={<HomePage />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/hats" element={<HatsPage />} />
-          <Route path="/login" element={<LoginRegister />} />
+          <Route
+            path="/login"
+            element={
+              this.props.currentUser ? (
+                <Navigate replace to="/" />
+              ) : (
+                <LoginRegister />
+              )
+            }
+          />
         </Routes>
       </div>
     );
   }
 }
 
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
